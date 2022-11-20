2d711c96d4bb8aebb6517c74bfd3809e109af365
ramverma-git <vermaram2021@gmail.com>
2022-11-20 14:04:39 +0530

    token script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,30 +1,95 @@
-import { HardhatUserConfig } from "hardhat/config";
-import { config as dotEnvConfig } from "dotenv";
+import { HardhatUserConfig, task } from "hardhat/config";
+import { config } from "dotenv";
 
-// plugins
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
 import "@nomiclabs/hardhat-waffle";
-import "solidity-coverage";
-import "hardhat-gas-reporter";
+import "@nomiclabs/hardhat-web3";
+import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
 import "@openzeppelin/hardhat-upgrades";
+
+// using community plugins
+import "solidity-coverage";
+import "hardhat-gas-reporter";
 import "hardhat-contract-sizer";
+import "hardhat-deploy";
 
-dotEnvConfig();
+// import {
+//   getFeeWalletAddress,
+//   getMasterWallet,
+//   getTrustForwarder,
+// } from "./config";
 
-const config: HardhatUserConfig = {
+config();
+
+const accounts = [process.env.PRIVATE_KEY] as string[];
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const hardhatConfig: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
-    goerli: {
-      url: process.env.GOERLI_RPC_URL,
-      accounts: [process.env.ADMIN_PRIVATE_KEY as string],
-      chainId: 5,
+    hardhat: {
+      accounts: {
+        count: 150,
+      },
     },
-  },
-  mocha: {
-    timeout: 100000,
-    color: true,
+    localhost: {
+      url: "http://localhost:8545",
+      saveDeployments: true,
+      loggingEnabled: true,
+      live: true,
+      tags: ["testnet", "staging"],
+    },
+    polygon: {
+      url: process.env.POLYGON_RPC_URL,
+      accounts,
+      saveDeployments: true,
+      loggingEnabled: true,
+      live: true,
+      tags: ["polygon", "mainnet"],
+      verify: {
+        etherscan: {
+          apiKey: "FSYHBPSIYA743ZKG6ECWM9753AN5KG8P8U",
+        },
+      },
+    },
+    ethereum: {
+      url: process.env.ETH_RPC_URL,
+      accounts,
+      saveDeployments: true,
+      loggingEnabled: true,
+      live: true,
+      tags: ["eth-testnet", "staging"],
+      verify: {
+        etherscan: {
+          apiKey: process.env.ETH_API_KEY,
+        },
+      },
+    },
+    bsc: {
+      url: process.env.BSC_RPC_URL,
+      accounts,
+      saveDeployments: true,
+      loggingEnabled: true,
+      live: true,
+      tags: ["bsc-testnet", "staging"],
+    },
   },
   solidity: {
     version: "0.8.10",
@@ -35,8 +100,54 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+      4: 0,
+      56: 0,
+      97: 0,
+      137: 0,
+    },
+    // feeWallet: {
+    //   1: getFeeWalletAddress(),
+    //   4: getFeeWalletAddress(),
+    //   56: getFeeWalletAddress(),
+    //   97: getFeeWalletAddress(),
+    //   137: getFeeWalletAddress(),
+    // },
+    // masterWallet: {
+    //   1: getMasterWallet(),
+    //   4: getMasterWallet(),
+    //   56: getMasterWallet(),
+    //   97: getMasterWallet(),
+    //   137: getFeeWalletAddress(),
+    // },
+    // trustForwarder: {
+    //   1: getTrustForwarder(),
+    //   4: getTrustForwarder(),
+    //   56: getTrustForwarder(),
+    //   97: getTrustForwarder(),
+    //   137: getFeeWalletAddress(),
+    // },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 20000,
+    color: true,
+  },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: process.env.ETH_API_KEY,
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: Boolean(process.env.REPORT_GAS),
+    coinmarketcap: process.env.CMC_API_KEY,
   },
   contractSizer: {
     alphaSort: true,
@@ -44,11 +155,6 @@ const config: HardhatUserConfig = {
     runOnCompile: true,
     strict: true,
   },
-  gasReporter: {
-    currency: "usd",
-    enabled: process.env.REPORT_GAS ? true : false,
-    coinmarketcap: process.env.CMC_API_KEY,
-  },
 };
 
-export default config;
+export default hardhatConfig;
